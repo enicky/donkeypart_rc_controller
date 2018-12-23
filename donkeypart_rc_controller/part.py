319469fda3527826a5556fa9ebb2c4ef848b15c5
nicholase <enicky@telenet.be>
2018-12-23 09:25:17 +0100

    updated everything to work with arduino implementation

--- donkeypart_rc_controller/part.py
+++ donkeypart_rc_controller/part.py
@@ -1,32 +1,68 @@
 import serial
 from time import sleep
+import sys
+import re
+#import datetime
+from datetime import datetime
+
+
+
+class Arduino:
+    import threading
+
+    arduino_device = None
+    arduino_lock = threading.Lock()
+
+    def __init__(self, channel, frequency=60):
+        self.frequency = frequency
+
+        if Arduino.arduino_device is None:
+            Arduino.arduino_device = serial.Serial('/dev/cu.usbserial-1410', 9600, timeout=0.01)
+        print("Arduino initialized")
+
+    def readLine(self):
+        ret = None
+        with Arduino.arduino_lock:
+            if Arduino.arduino_device.inWaiting() > 0:
+                ret = Arduino.arduino_device.readline()
+
+        if ret is not None:
+            ret = ret.rstrip()
+        return ret
+
 
 
 class RcController:
 
     def __init__(self, hz=20):
-        dev = '/dev/ttyACM0' #arduino
+        self.inSteering = 0.0
+        self.inThrottle = 0.0
 
-        self.ser = serial.Serial(dev, 57600)
+        self.sensor = Arduino(0)
+
+        self.num_channels = 2
         print("Initialized Serial Port")
         self.running = True
         self.hz = hz
         self.sleep_time = 1000/float(self.hz) / 1000.0
+        print("Sleep Time " + str(self.sleep_time))
 
 
     def getLatestStatus(self):
         ''' This clears the buffer and returns only the last value
             This is faster than polling the arduino for the latest value
         '''
-        status = b''
-        while self.ser.inWaiting() > 0:
-            # read and discard any values except the most recent.
-            # when Arduino Hz = 2x python Hz this results in 1 to 2
-            # discarded results
-            status = self.ser.readline()
+        status = self.sensor.readLine()
+        if status is None:
+            return ['0' for x in range(self.num_channels)]
+
+        decoded = status.strip().decode("utf-8")
+        if  not decoded.startswith('B') and len(decoded.strip()) == 0:
+            vel_angle_raw = ['0' for x in range(self.num_channels)]
+            return vel_angle_raw
 
         # return it as an array
-        # raw format = b'1234 1234\r\n' from the Arduino
+        # raw format = b'B 1234 1234\r\n' from the Arduino
         try:
             vel_angle_raw = status.strip().decode("utf-8").split(" ")
         except Exception:
@@ -36,21 +72,40 @@ class RcController:
             vel_angle_raw = ['0' for x in range(self.num_channels)]
         return vel_angle_raw
 
+
+
     def update(self):
         """ Start the threa for reading the arduino """
+        print("Start Update Thread")
         while self.running:
-            vel_angle_raw = self.getLatestStatus()
+            start = datetime.now()
 
-            if(len(vel_angle_raw) == 2):
-                print(list(map(int, vel_angle_raw)))
+            p = self.getLatestStatus()
+            if len(p) != 2:
+                i = float(p[1])
+                k = float(p[2])
 
-            sleep(self.sleep_time)
+                self.inSteering = i * 1.0 / 100.0
+                self.inThrottle = k * 1.0 / 100.0
 
-    def run(self):
-        print("run")
+                #print("matched %.2f  %.2f  %.2f  %.2f" % (i, (self.inSteering), k, (self.inThrottle)))
+            stop = datetime.now()
+            s = 0.01 - (stop - start).total_seconds()
+            if s > 0:
+                sleep(s)
+
+    def run_threaded(self):
+        return self.inSteering, self.inThrottle
 
     def shutdown(self):
         self.running = False
         print("Stopping rc parsing thread")
         sleep(.5)
-        self.ser.close()
+
+
+if __name__ == "__main__":
+    '''
+    publish rc controller
+    '''
+    p = RcController()
+    p.update()
